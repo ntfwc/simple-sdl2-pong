1939e701fea1bc54e1ced53690fb4c4221f1d4d6
ntfwc <ntfwc@yahoo.com>
2018-03-19 22:51:50 -0500

    Started main loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,33 @@
 #include "SdlSession.hpp"
 #include "SdlWindow.hpp"
 
+void mainLoop(SDL_Renderer* renderer)
+{
+	const SDL_Rect rect = {100, 100, 100, 100};
+
+	bool running = true;
+	SDL_Event e;
+	while (running)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				running = false;
+			}
+		}
+		
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_RenderFillRect(renderer, &rect);
+
+		SDL_RenderPresent(renderer);
+		SDL_Delay(100);
+	}
+}
+
 int main()
 {
 	SdlSession session;
@@ -12,6 +39,6 @@ int main()
 	SdlWindow window;
 	if (!window.init("Pong"))
 		return 2;
-	SDL_Delay(2000);
+	mainLoop(window.getRenderer());
 	return 0;
 }
