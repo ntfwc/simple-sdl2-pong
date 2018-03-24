729cd5516cf214ccbe8238f548e422435d68b968
ntfwc <ntfwc@yahoo.com>
2018-03-24 16:31:05 -0500

    Updated the wine test script

--- wine-test-mingw-build.sh
+++ wine-test-mingw-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 C_DLL_PATH="/usr/lib/gcc/x86_64-w64-mingw32/5.3-win32"
 SDL2_DLL_PATH="/usr/local/custom/mingw/SDL2/x86_64-w64-mingw32/bin"
-export WINEPATH="$SDL2_DLL_PATH;$C_DLL_PATH"
+SDL2_ttf_DLL_PATH="/usr/local/custom/mingw/SDL2_ttf/x86_64-w64-mingw32/bin"
+export WINEPATH="$SDL2_DLL_PATH;$SDL2_ttf_DLL_PATH;$C_DLL_PATH"
 
 wine build-mingw/pong.exe
