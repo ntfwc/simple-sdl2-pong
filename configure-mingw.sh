df99ca15dd8b190cd492676a15280e18738a4c87
ntfwc <ntfwc@yahoo.com>
2018-03-24 15:58:56 -0500

    Added a script to configure a mingw build. Fixed a few things in the cmake configuration

---
+++ configure-mingw.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+[ -e build-mingw ] || mkdir build-mingw
+cd build-mingw
+
+MINGW_SDL2_DIR="/usr/local/custom/mingw/SDL2"
+
+REL_PKG_PATH="x86_64-w64-mingw32/lib/pkgconfig"
+
+SDL2_PKG_PATH="$MINGW_SDL2_DIR/$REL_PKG_PATH"
+
+export PKG_CONFIG_PATH="$SDL2_PKG_PATH"
+
+export CC="/usr/bin/x86_64-w64-mingw32-gcc"
+export CXX="/usr/bin/x86_64-w64-mingw32-g++"
+export CFLAGS="-pipe -O2"
+export CXXFLAGS="$CFLAGS"
+
+cmake -DCMAKE_SYSTEM_NAME=Windows -G Ninja ..
