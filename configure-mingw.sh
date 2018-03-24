68b49031e11402a8bce168ed53b347fb857aacb9
ntfwc <ntfwc@yahoo.com>
2018-03-24 16:25:16 -0500

    Added SDL2_ttf path to mingw configure script

--- configure-mingw.sh
+++ configure-mingw.sh
@@ -4,12 +4,14 @@ set -e
 cd build-mingw
 
 MINGW_SDL2_DIR="/usr/local/custom/mingw/SDL2"
+MINGW_SDL2_ttf_DIR="/usr/local/custom/mingw/SDL2_ttf"
 
 REL_PKG_PATH="x86_64-w64-mingw32/lib/pkgconfig"
 
 SDL2_PKG_PATH="$MINGW_SDL2_DIR/$REL_PKG_PATH"
+SDL2_ttf_PKG_PATH="$MINGW_SDL2_ttf_DIR/$REL_PKG_PATH"
 
-export PKG_CONFIG_PATH="$SDL2_PKG_PATH"
+export PKG_CONFIG_PATH="$SDL2_PKG_PATH:$SDL2_ttf_PKG_PATH"
 
 export CC="/usr/bin/x86_64-w64-mingw32-gcc"
 export CXX="/usr/bin/x86_64-w64-mingw32-g++"
